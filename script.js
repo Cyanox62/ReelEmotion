e581069baf12bc4a0a46468de96290c6cd3e2ca4
pujal0909 <PUG9@pitt.edu>
2024-04-22 16:40:53 -0400

    updates to script.hs

--- script.js
+++ script.js
@@ -11,7 +11,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
         // - connect to the API here (5 mins -- once we have gcf link)
         // - take the API feedback and format it onto the page (probs like 5 mins)
         const CLOUD_FUNCTION_URL = "https://us-central1-steel-cairn-418322.cloudfunctions.net/query-sentiment-analysis";
+        fetch(CLOUD_FUNCTION_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title: value })
+        
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed');
+            }
+            return response.json(); 
+        })
+        .then(data => {
+            console.log(data); 
+        })
+        .catch(error => {
+            console.error('Error:', error.message);
+        });
+        
     });
+    
 });
 
 function getValue() {
